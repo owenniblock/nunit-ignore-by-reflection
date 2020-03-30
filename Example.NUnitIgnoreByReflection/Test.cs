6116a565594a4a0f294b008e75b5efdd26b17954
Owen Niblock <me@owenniblock.com>
2020-03-30 13:06:59 +0100

    Code for demo

---
+++ Example.NUnitIgnoreByReflection/Test.cs
@@ -0,0 +1,78 @@
+﻿using Example.ReflectionHelper;
+using NUnit.Framework;
+using System;
+namespace Example.NUnitIgnoreByReflection
+{
+    [TestFixture("Run")]
+    [TestFixture("Ignore")]
+    [TestFixture("Inconclusive")]
+    [TestFixture("IgnoreByReflection")]
+    [TestFixture("InconclusiveByReflection")]
+    public class Test
+    {
+        private string _ignoreOrInconclusive;
+
+        public Test(string ignoreOrInconclusive)
+        {
+            _ignoreOrInconclusive = ignoreOrInconclusive;
+
+            // The code works when we call it in this method (construction of the Test Class).
+            var methodInvoker = new MethodInvoker();
+
+            switch (_ignoreOrInconclusive)
+            {
+                case "Ignore":
+                    Assert.Ignore("Ignored directly :-)");
+                    break;
+                case "Inconclusive":
+                    Assert.Inconclusive("Who knows what happened here?");
+                    break;
+                case "IgnoreByReflection":
+                    methodInvoker.Invoke("Ignore", "I am ignoring you, but not directly :-p");
+                    break;
+                case "InconclusiveByReflection":
+                    methodInvoker.Invoke("Inconclusive", "Results are inconclusive at this time :-o");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        [SetUp]
+        public virtual void BeforeEachTest()
+        {
+            // Whereas this code fails for the Reflection cases (SetUp called before each test).
+            //var methodInvoker = new MethodInvoker();
+
+            //switch (_ignoreOrInconclusive)
+            //{
+            //    case "Ignore":
+            //        Assert.Ignore("Ignored directly :-)");
+            //        break;
+            //    case "Inconclusive":
+            //        Assert.Inconclusive("Who knows what happened here?");
+            //        break;
+            //    case "IgnoreByReflection":
+            //        methodInvoker.Invoke("Ignore", "I am ignoring you, but not directly :-p");
+            //        break;
+            //    case "InconclusiveByReflection":
+            //        methodInvoker.Invoke("Inconclusive", "Results are inconclusive at this time :-o");
+            //        break;
+            //    default:
+            //        break;
+            //}
+        }
+
+        [Test()]
+        public void PassingTestCase()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test()]
+        public void FailingTestCase()
+        {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
---
+++ Example.ReflectionHelper/MethodInvoker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Example.ReflectionHelper
+{
+    public class MethodInvoker
+    {
+        public void Invoke(string methodName, string exceptionText)
+        {
+            var nunitType = FindAssertType("NUnit.Framework.Assert", exceptionText);
+            var method = nunitType.GetMethod(methodName, new[] { typeof(string) });
+
+            if (method == null)
+            {
+                throw new Exception(exceptionText);
+            }
+
+            method.Invoke(null, new object[] { exceptionText });
+        }
+
+        private Type FindAssertType(string frameworkTypeName, string exceptionText)
+        {
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies().Where(a => a.FullName.StartsWith("nunit.framework")))
+            {
+                var type = assembly.GetExportedTypes().FirstOrDefault(t => t.FullName == frameworkTypeName);
+                if (type != null)
+                {
+                    return type;
+                }
+            }
+
+            throw new Exception(exceptionText);
+        }
+    }
+}
